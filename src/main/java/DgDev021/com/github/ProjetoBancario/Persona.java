f2f25d5cba164014e66a7897ddaa28f3fcb2d081
Douglas Guedes <douglasgabrielguedes@gmail.com>
2024-03-04 21:04:22 -0300

    configuração e criação da classe persona

--- src/main/java/DgDev021/com/github/ProjetoBancario/Persona.java
+++ src/main/java/DgDev021/com/github/ProjetoBancario/Persona.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
@@ -13,26 +12,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
-@Data //Cria todos os getter, setters, toString e hashCode
-@Builder
-@Entity //Isso define que é uma classe do banco de dados
-@NoArgsConstructor
-@AllArgsConstructor
+
+@Data     //Gera o Get, Set, Equals e HashCode
+@Builder  //Habilita a criação de um objeto staticamente
+@Entity   //Isso aqui define que é uma classe do banco de dados
+@NoArgsConstructor  //Cria um construtor vazio
+@AllArgsConstructor //Cria um construtor com todos parametros
 public class Persona {
 
-  @Id //Define a primary key do banco de dados
-  @GeneratedValue(strategy = GenerationType.IDENTITY) //Define o tipo de gerador
+  @Id //Define qual é a primary Key do banco de dados
+  @GeneratedValue(strategy = GenerationType.IDENTITY) //Define o tipo de gerador do PK
   private Long id;
 
   @Column(name = "name", nullable = false) //Define o padrão para a coluna
   private String name;
 
-  @Email
+  @Email  //Faz a validação do tipo email (@ .com)
   private String email;
 
   private String password;
 
-  @CPF
+  @CPF  //Faz a validação matematica do CPF
   private String cpf;
 
   private LocalDate birthDay;
